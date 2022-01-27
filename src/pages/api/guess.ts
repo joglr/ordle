377f0b842def8f664761fa293e8abf1dd43d6cbb
Jonas Røssum <1959615+joglr@users.noreply.github.com>
2022-01-27 22:26:07 +0100

    Refactor guess API

--- src/pages/api/guess.ts
+++ src/pages/api/guess.ts
@@ -1,58 +1,55 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
-import { colorize, BoardState, OrdleState, LoadingState } from "../../state";
+import {
+  colorize,
+  BoardState,
+  OrdleState,
+  LoadingState,
+  createEmptyState,
+} from "../../state";
 import { getTodaysWord, isWord } from "../../word";
 
-export interface GuessSuccessResponse extends OrdleState {
-  board: BoardState;
-  loadingState: LoadingState.SUCCESS;
-  responseText: null;
-}
-
-export interface GuessErrorResponse extends OrdleState {
-  board: BoardState;
-  loadingState: LoadingState.ERROR;
-  responseText: string;
-}
-
-export type GuessResponseBody = GuessSuccessResponse | GuessErrorResponse;
-
 type GuessResponse = Omit<VercelResponse, "json"> & {
-  json: (arg0: GuessResponseBody) => VercelResponse;
+  json: (arg0: OrdleState) => VercelResponse;
 };
 
 const todaysWord = getTodaysWord();
 
 export default function guess(req: VercelRequest, res: GuessResponse) {
-  // TODO: Stop logging todays word
-  console.log("todaysWord: ", todaysWord);
+  try {
+    const state = req.body as OrdleState;
 
-  const { board } = req.body as unknown as OrdleState;
+    if (state === undefined) {
+      return res.json({
+        ...createEmptyState(),
+        loadingState: LoadingState.ERROR,
+        responseText: "Intern fejl",
+      });
+    }
 
-  if (board === undefined) {
+    const word = state.board.currentAttempt.join("");
+    const wordLowercase = word.toLowerCase();
+
+    const wordIsWord = isWord(wordLowercase);
+
+    if (!wordIsWord)
+      return res.json({
+        loadingState: LoadingState.ERROR,
+        responseText: `${word} er ikke et gyldigt ord`,
+        board: state.board,
+      });
+
+    const newBoardState = colorize(state.board, todaysWord);
+
+    return res.json({
+      loadingState: LoadingState.SUCCESS,
+      board: newBoardState,
+      responseText: null,
+    });
+  } catch (e) {
     return res.json({
       loadingState: LoadingState.ERROR,
       responseText: "Intern fejl",
-      board,
+      board: createEmptyState().board,
     });
   }
-
-  const word = board.currentAttempt.join("");
-  const wordLowercase = word.toLowerCase();
-
-  const wordIsWord = isWord(wordLowercase);
-
-  if (!wordIsWord)
-    return res.json({
-      loadingState: LoadingState.ERROR,
-      responseText: `${word} er ikke et gyldigt ord`,
-      board,
-    });
-
-  const newBoardState = colorize(board, todaysWord);
-
-  return res.json({
-    loadingState: LoadingState.SUCCESS,
-    board: newBoardState,
-    responseText: null,
-  });
 }
