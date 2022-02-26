bb3bd38fd020963da8b31fa8cf8c935392139b2d
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2022-02-26 23:06:06 +0100

    Fix mui installation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,26 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import { ThemeProvider, CssBaseline } from "@mui/material";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import createEmotionCache from "../utility/create-emotion-cache";
+import { darkTheme } from "../styles/theme/themes";
+import "../styles/globals.css";
+import { AppProps } from "next/app";
+
+const clientSideEmotionCache = createEmotionCache();
+
+function MyApp({
+  Component,
+  emotionCache = clientSideEmotionCache,
+  pageProps,
+}: AppProps & { emotionCache?: EmotionCache }) {
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
 
 export default MyApp;
