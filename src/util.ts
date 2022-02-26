4dc293037875102df6e1ca75a4bd9cf27ec4b223
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2022-02-26 22:09:14 +0100

    Logging (#29)
    * Improve logging

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,6 @@
 export function clone<T>(o: T) {
   return JSON.parse(JSON.stringify(o)) as T;
 }
-export const last = (a: any[]) => a[a.length - 1];
-export const isLast = (a: any[], i: number) => i === a.length - 1;
+export const first = (arr: unknown[]) => arr[0];
+export const last = (arr: unknown[]) => arr[arr.length - 1];
+export const isLast = (arr: unknown[], i: number) => i === arr.length - 1;
