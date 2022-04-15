f1cbd1cb180b28816b969a5ace7fd2d0280a7e37
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2022-04-15 14:29:36 +0200

    Invert condition

--- src/hooks.ts
+++ src/hooks.ts
@@ -3,11 +3,11 @@ import { useEffect } from "react";
 export function useUpdateAvailable(handler: () => void) {
   useEffect(() => {
     if (
-      typeof window !== "undefined" &&
-      "serviceWorker" in navigator &&
+      typeof window === "undefined" ||
+      "serviceWorker" in navigator ||
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       //@ts-ignore
-      window.workbox !== undefined
+      window.workbox === undefined
     )
       return;
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
